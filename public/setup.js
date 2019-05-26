ca8b3bbdbfd41beeeef5bc067549805fd301a746
Xhika <laureta.dzika@gmail.com>
2019-05-26 21:49:53 +0200

    added waves
    enemies now comes in waves

--- public/setup.js
+++ public/setup.js
@@ -5,10 +5,11 @@ function setup() {
 	// Create and position Dino
 	dino = createSprite(width / 2, GROUND);
 	dino.scale = .3;
-	dino.addAnimation('animate', walkingDino);
+	dino.addAnimation('walk', walkingDino)
 	dino.setCollider("rectangle", 0, 5, 204, 390);
 	dino.addAnimation('dead', deadDino)
 	dino.addAnimation('jump', jumpingDino)
+	dino.addAnimation('run', runningDino)
 
 	// Create fireball
 	fireBalls = createSprite(-100, -100)
@@ -20,14 +21,18 @@ function setup() {
 	nyanCat.setSpeed(random(2, 50), 180)
 	nyanCat.mirrorX(-1);
 
-	spawningEnemies();
+	spawningEnemies(0);
 }
 
-function spawningEnemies() {
-	for (let i = 0; i < ENEMYCOUNT; i++) {
-		let randomEnemySpeed = random(1, 7);
+function spawningEnemies(waveIndex) {
+	KILLCOUNT = 0
+
+	let WAVE = WAVES[waveIndex]
+	console.log('WAVE ' + waveIndex)
+	for (let i = 0; i < WAVE.count; i++) {
+		let randomEnemySpeed = random(WAVE.speed.min, WAVE.speed.max);
 		let enemy = createSprite(width + 500, GROUND);
-		enemy.position.x += random(200, 7000);
+		enemy.position.x += random(1200, 1500);
 		enemy.scale = random(0.2, 0.5);
 		enemy.addAnimation('animate', walkingEnemy);
 		enemy.setCollider("rectangle", 20, 5, 204, 390);
@@ -35,4 +40,5 @@ function spawningEnemies() {
 		enemy.setSpeed(randomEnemySpeed, 180)
 		enemies.push(enemy)
 	};
+	LASTWAVE = waveIndex;
 }
